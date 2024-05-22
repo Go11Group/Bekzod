de0eca9162ca1b4206e356600e4da3390c0e83d8
bekzod <qochqarovbekzod049@gmail.com>
2024-05-22 16:34:41 +0500

    xususiyat1

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	for i := 0; i < 8; i++ {
-		fmt.Println(i)
-	}
-}
---
+++ main2.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main()  {
+	k:=6
+	for i := 0; i < k; i++ {
+		fmt.Println(i*k)
+	}
+}
